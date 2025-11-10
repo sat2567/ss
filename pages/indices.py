8ca61d06da41819bcc4684cb2633f5946b5b1259
satya <97119086+sat2567@users.noreply.github.com>
2025-11-10 14:18:12 +0530

    Update NIFTY 50 CSV URL to GitHub link

--- pages/indices.py
+++ pages/indices.py
@@ -5,7 +5,7 @@ import plotly.graph_objs as go
 # CSV data URLs
 csv_files = {
     "NIFTY BANK": "https://raw.githubusercontent.com/sat2567/ss/my-new-branch/NIFTY%20BANK-29-09-2024-to-29-09-2025.csv",
-    "NIFTY 50": "https://raw.githubusercontent.com/sat2567/ss/my-new-branch/NIFTY%2050-29-09-2024-to-29-09-2025.csv",
+    "NIFTY 50": "https://github.com/sat2567/ss/blob/my-new-branch/NIFTY%2050-10-10-2025-to-10-11-2025.csv",
     "NIFTY MIDCAP 100": "https://raw.githubusercontent.com/sat2567/ss/my-new-branch/NIFTY%20MIDCAP%20100-29-09-2024-to-29-09-2025.csv",
     "NIFTY SMALLCAP 100": "https://raw.githubusercontent.com/sat2567/ss/my-new-branch/NIFTY%20SMALLCAP%20100-29-09-2024-to-29-09-2025.csv"
 }
